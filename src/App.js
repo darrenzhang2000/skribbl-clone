96d8a5b50a37a5905b1ef918d085df5d362dca40
Darren Zhang <darrenzhang200027@gmail.com>
2020-03-20 21:42:56 -0400

    added usercard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './components/home/Home'
+import User from './components/home/User';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <User/>
     </div>
   );
 }
---
+++ src/components/home/User.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const User = (props) => <div>
+    <input type='text' placeholder='username'/>
+    <p>Image</p>
+</div>
+
+export default User
