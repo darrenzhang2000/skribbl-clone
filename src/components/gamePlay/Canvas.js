9aed4e9e32d0c8d591b3bda846c6bbfa5e88e157
Darren Zhang <darrenzhang200027@gmail.com>
2020-03-24 22:30:12 -0400

    got rid of unnecessary code

--- src/components/gamePlay/Canvas.js
+++ src/components/gamePlay/Canvas.js
@@ -12,9 +12,6 @@ function Canvas() {
     return (
         <div id="container">
             <DrawArea />
-            <svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
-                <path d="M 10 10 H 90 V 90 H 10 " />
-            </svg>
         </div>
     )
 }
