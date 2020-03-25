87fa80ed03bcaa010e3c4a42608c79df1ee445bb
Darren Zhang <darrenzhang200027@gmail.com>
2020-03-24 22:29:32 -0400

    added canvas

--- src/components/gamePlay/Canvas.js
+++ src/components/gamePlay/Canvas.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { Component } from "react"
+import DrawArea from "./drawing/DrawArea"
+import "./Canvas.css"
 
-let canvasStyle = {
-    border: '2px solid #f505f5',
-    width: '200px',
-    margin: 'auto'
+// let canvasStyle = {
+//     border: "2px solid #f505f5",
+//     width: "200px",
+//     margin: "auto"
+// }
+
+function Canvas() {
+    return (
+        <div id="container">
+            <DrawArea />
+            <svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
+                <path d="M 10 10 H 90 V 90 H 10 " />
+            </svg>
+        </div>
+    )
 }
-
-const Canvas = props => <div style={canvasStyle}>
-    <h1>Canvas</h1>
-</div>
 
 export default Canvas
