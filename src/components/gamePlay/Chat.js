f0965b6ac1f08bf49773d77d0bd24af659dca152
Darren Zhang <darrenzhang200027@gmail.com>
2020-03-26 23:58:06 -0400

    added messageChangeHandler

--- src/components/gamePlay/Chat.js
+++ src/components/gamePlay/Chat.js
@@ -1,5 +1,6 @@
-import React from "react"
-import './Chat.css'
+import React, { Component } from "react"
+import "./Chat.css"
+import socketIOClient from "socket.io-client";
 
 /*
     onClickHandler - emit message of type 'chat' to backend
@@ -10,17 +11,47 @@ import './Chat.css'
     question: how to have sockets constantly listen? 
 */
 
+class Chat extends Component {
+    constructor() {
+        super();
+        this.state = {
+          response: false,
+          endpoint: "http://127.0.0.1:4001",
+          message: ""
+        };
+        this.textInput = React.createRef()
+      }
+    componentDidMount = () => {
+        const { endpoint } = this.state;
+        const socket = socketIOClient(endpoint);
+        
+    }
 
-const Chat = props => (
-    <div id="chat">
-        <div id="chat-window">
-            <div id="output"></div>
-            <div id="feedback"></div>
-        </div>
-        <input id="handle" type="text" placeholder="Handle" />
-        <input id="message" type="text" placeholder="Message" />
-        <button id="send">Send</button>
-    </div>
-)
+    sendButtonHandler = () => {
+        console.log('pomegranate')
+        // this.state.socket.emit('chat', {
+        //     handle: handle.value,
+        //     message: message.value
+        // })
+    }
+    
+    messageChangeHandler = e =>{
+        this.setState({message: e.target.value})
+        console.log(this.state.message)
+    }
+    render() {
+        return (
+            <div id="chat">
+                <div id="chat-window">
+                    <div id="output"></div>
+                    <div id="feedback"></div>
+                </div>
+                <input id="handle" type="text" placeholder="Handle" />
+                <input id="message" type="text" placeholder="Message" onChange={this.messageChangeHandler} value={this.state.message}/>
+                <button id="send" onClick={this.sendButtonHandler}>Send</button>
+            </div>
+        )
+    }
+}
 
 export default Chat
